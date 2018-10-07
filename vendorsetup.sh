d91e6b5528afd5fa0be8e3254f372e86483b3037
Askuccio <valelinux7@gmail.com>
2018-10-07 13:37:40 +0200

    Fix typo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,8 +2,6 @@ if [[ -e vendor/aosp ]]; then
     add_lunch_combo aosp_hi6250-userdebug
 elif [[ -e vendor/lineage ]]; then
     add_lunch_combo lineage_hi6250-userdebug
-elif [[ -e vendor/cm ]]; then
-    add_lunch_combo pixys_hi6250-userdebug
 elif [[ -e vendor/pixys ]]; then
     add_lunch_combo pixys_hi6250-userdebug
 fi
