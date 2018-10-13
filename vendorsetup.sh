fbf4127ab41eb4e534f2ca7d681d33127faa3e00
Ramy ahmed <ramy@ahmedramy.com>
2018-10-13 09:52:11 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,4 +4,6 @@ elif [[ -e vendor/lineage ]]; then
     add_lunch_combo lineage_hi6250-userdebug
 elif [[ -e vendor/pixys ]]; then
     add_lunch_combo pixys_hi6250-userdebug
+else
+    add_lunch_combo aosp_hi6250-userdebug
 fi
